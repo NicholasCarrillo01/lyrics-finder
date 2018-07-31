2af3c8344b71f84aaba0d9b6b04db6c8ac9587ac
Nicholas Carrillo <nicholasmcxbox@gmail.com>
2018-07-31 11:51:48 -0400

    added api url

--- lyrics finder/ViewController.swift
+++ lyrics finder/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var artistNameTextField: UITextField!
+    @IBOutlet weak var songNameTextField: UITextField!
+    @IBOutlet weak var lyricsTextView: UITextView!
+    
+    let baseURL  = "https://api.lyrics.ovh/v1/"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func submitButtonTapped(_ sender: Any) {
+    }
+    
+    
 }
 
